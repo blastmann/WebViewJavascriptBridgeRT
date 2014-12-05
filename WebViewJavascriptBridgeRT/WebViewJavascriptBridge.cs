35d5f24a2eed18fe2768f25f5fd5cbd5fe3a3825
chaoming.edc <chaoming.edc@outlook.com>
2014-12-05 21:13:11 +0800

    implement some other api

--- WebViewJavascriptBridgeRT/WebViewJavascriptBridge.cs
+++ WebViewJavascriptBridgeRT/WebViewJavascriptBridge.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Json;
@@ -10,14 +11,17 @@ using Windows.UI.Xaml.Controls;
 
 namespace WebViewJavascriptBridgeRT
 {
-	public delegate void WVJBResponseCallBack(object data);
-	public delegate void WVJBHandler(object data, WVJBResponseCallBack responseCallBack);
+	public delegate void WVJBResponseCallback(string data);
+	public delegate void WVJBHandler(string data, WVJBResponseCallback responseCallback);
 	public sealed class WebViewJavascriptBridge
 	{
+		private const string KCustomProtocolScheme = "wvjbscheme";
+		private const string KQueueHasMessage = "__WVJB_QUEUE_MESSAGE__";
+
 		private WeakReference<WebView> _webViewReference;
 		private WVJBHandler _messageHandler;
 		private List<Dictionary<string, string>> _startupMessageQueue;
-		private Dictionary<string, WVJBResponseCallBack> _responseCallbacks;
+		private Dictionary<string, WVJBResponseCallback> _responseCallbacks;
 		private Dictionary<string, WVJBHandler> _messageHandlers;
 		private long _uniqueId;
 		private ulong _numRequestsLoading;
@@ -32,7 +36,7 @@ namespace WebViewJavascriptBridgeRT
 			this.Send(message, null);
 		}
 
-		public void Send(object message, WVJBResponseCallBack responseCallBack)
+		public void Send(object message, WVJBResponseCallback responseCallback)
 		{
 
 		}
@@ -52,18 +56,74 @@ namespace WebViewJavascriptBridgeRT
 
 		}
 
-		public void callHandler(string handlerName, object data, WVJBResponseCallBack responseCallBack)
+		public void callHandler(string handlerName, object data, WVJBResponseCallback responseCallback)
 		{
 
 		}
 
 		private void Setup(WebView webView, WVJBHandler handler)
 		{
+			webView.ScriptNotify += WebViewOnScriptNotify;
+			webView.NavigationStarting += WebViewOnNavigationStarting;
+			webView.NavigationFailed += WebViewOnNavigationFailed;
+			webView.NavigationCompleted += WebViewOnNavigationCompleted;
+
 			_webViewReference = new WeakReference<WebView>(webView);
 			_messageHandler = handler;
 		}
 
-		private void SendData(string data, WVJBResponseCallBack responseCallBack, string handlerName)
+		private async void WebViewOnNavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+		{
+			_numRequestsLoading--;
+
+			if (_numRequestsLoading == 0 && await sender.InvokeScriptAsync("eval", new[] { @"typof WebViewJavascriptBridge == 'object'" }) == "true")
+			{
+				// todo: load js from file
+				await sender.InvokeScriptAsync("eval", new[] { "" });
+			}
+
+			var startupMessageQueue = _startupMessageQueue;
+			_startupMessageQueue = null;
+
+			if (startupMessageQueue != null)
+			{
+				foreach (var message in startupMessageQueue)
+				{
+					DispatchMessage(message);
+				}
+			}
+		}
+
+		private void WebViewOnNavigationFailed(object sender, WebViewNavigationFailedEventArgs webViewNavigationFailedEventArgs)
+		{
+			_numRequestsLoading--;
+		}
+
+		private void WebViewOnNavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+		{
+			_numRequestsLoading++;
+		}
+
+		private void WebViewOnScriptNotify(object sender, NotifyEventArgs notifyEventArgs)
+		{
+			var notifyMessage = notifyEventArgs.Value;
+			if (string.IsNullOrEmpty(notifyMessage))
+				return;
+
+			var url = new Uri(notifyMessage);
+			if (url.Scheme != KCustomProtocolScheme) return;
+
+			if (url.Host == KQueueHasMessage)
+			{
+				FlushMessage();
+			}
+			else
+			{
+				Debug.WriteLine("WebViewJavascriptBridge: WARNING: Received unknown WebViewJavascriptBridge command {0}://{1}", KCustomProtocolScheme, url.PathAndQuery);
+			}
+		}
+
+		private void SendData(string data, WVJBResponseCallback responseCallback, string handlerName)
 		{
 			var message = new Dictionary<string, string>();
 			if (data != null)
@@ -71,11 +131,11 @@ namespace WebViewJavascriptBridgeRT
 				message.Add("data", data);
 			}
 
-			if (responseCallBack != null)
+			if (responseCallback != null)
 			{
 				_uniqueId++;
 				string callbackId = "cb_" + _uniqueId;
-				_responseCallbacks.Add(callbackId, responseCallBack);
+				_responseCallbacks.Add(callbackId, responseCallback);
 				message.Add("callbackId", callbackId);
 			}
 
@@ -95,6 +155,84 @@ namespace WebViewJavascriptBridgeRT
 			else
 			{
 				DispatchMessage(message);
+			}
+		}
+
+		private async void FlushMessage()
+		{
+			WebView webView;
+			if (!_webViewReference.TryGetTarget(out webView))
+				return;
+
+			var messageQueueString = await webView.InvokeScriptAsync("WebViewJavascriptBridge._fetchQueue();", null);
+			if (string.IsNullOrEmpty(messageQueueString))
+				return;
+
+			var messages = DeserializeMessage(messageQueueString);
+			if (messages == null)
+			{
+				Debug.WriteLine("WebViewJavascriptBridge: WARNING: Invalid received");
+				return;
+			}
+
+			foreach (var message in messages)
+			{
+				string responseId;
+				string data;
+				WVJBResponseCallback responseCallback;
+				if (message.TryGetValue("responseId", out responseId) && message.TryGetValue("responseData", out data))
+				{
+					if (_responseCallbacks.TryGetValue(responseId, out responseCallback))
+					{
+						responseCallback(data);
+						_responseCallbacks.Remove(responseId);
+					}
+				}
+				else
+				{
+					string callbackId;
+					if (message.TryGetValue("callbackId", out callbackId))
+					{
+						responseCallback = delegate(string responseData)
+						{
+							if (string.IsNullOrEmpty(responseData))
+							{
+								responseData = string.Empty;
+							}
+
+							var msg = new Dictionary<string, string>
+							{
+								{"responseId", callbackId},
+								{"responseData", responseData}
+							};
+							QueueMessage(msg);
+						};
+					}
+					else
+					{
+						responseCallback = s => Debug.WriteLine("Empty callback");
+					}
+
+					WVJBHandler handler;
+					string handlerName;
+					if (message.TryGetValue("handlerName", out handlerName))
+					{
+						_messageHandlers.TryGetValue(handlerName, out handler);
+					}
+					else
+					{
+						handler = _messageHandler;
+					}
+
+					if (handler == null)
+						throw new Exception("No handler for message from JS:" + message);
+
+					string messageData;
+					if (message.TryGetValue("data", out messageData))
+					{
+						handler(messageData, responseCallback);
+					}
+				}
 			}
 		}
 
@@ -149,12 +287,12 @@ namespace WebViewJavascriptBridgeRT
 			}
 		}
 
-		private Dictionary<string, string> DeserializeMessage(string message)
+		private IEnumerable<Dictionary<string, string>> DeserializeMessage(string message)
 		{
 			using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(message)))
 			{
-				var serializer = new DataContractJsonSerializer(typeof(Dictionary<string, string>));
-				return (Dictionary<string, string>)serializer.ReadObject(ms);
+				var serializer = new DataContractJsonSerializer(typeof(IEnumerable<Dictionary<string, string>>));
+				return serializer.ReadObject(ms) as IEnumerable<Dictionary<string, string>>;
 			}
 		}
 	}
